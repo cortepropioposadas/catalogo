ce2af559bbb23fbdc9daf3e748c47c1bbba5cf08
cortepropioposadas <cortepropioposadas@gmail.com>
2026-05-22 12:39:01 +0200

    Revise product details in productos.js
    Updated product descriptions, materials, and prices for various items in productos.js.

--- js/productos.js
+++ js/productos.js
@@ -7,8 +7,8 @@ const productos = [
         titulo: "Llavero Pokémon",
         precioUnidad: 3.50,
         precioPack: "29,75€ (Pack de 10 unid.) [-15%]",
-        descripcion: "Llavero con el logo y la figura de Pikachu ",
-        material: "---",
+        descripcion: "Llavero con el logo y la figura de Pikachu. diametro 5 cm",
+        material: "MDF lacado en blanco",
         tematica: ["llaveros", "friki"],
         imagen: "img/llaveropokemon.jpg"
     },
@@ -17,8 +17,8 @@ const productos = [
         titulo: "Abalorio cruz",
         precioUnidad: 1.50,
         precioPack: "13,50€ (Pack de 10 unid.) [-10%]",
-        descripcion: "Formas recortadas para llaveros, pulseras, collares... Podemos hacerlos en diferentes tipos de medidas y orificios.",
-        material: "---",
+        descripcion: "Formas recortadas para llaveros, pulseras, collares... Podemos hacerlos en diferentes tipos de medidas y orificios. tamaño aprox 3cm, modificable",
+        material: "MDF",
         tematica: ["llaveros", "abalorios", "Bautizo y comunión"],
         imagenes: [
             "img/abaloriocruz.jpg",
@@ -30,7 +30,7 @@ const productos = [
         titulo: "Abalorio mariposa",
         precioUnidad: 1.50,
         precioPack: "13,50€ (Pack de 10 unid.) [-10%]",
-        descripcion: "Formas recortadas para llaveros, pulseras, collares... Podemos hacerlos en diferentes tipos de medidas y orificios.",
+        descripcion: "Formas recortadas para llaveros, pulseras, collares... Podemos hacerlos en diferentes tipos de medidas y orificios.tamaño aprox 3cm ancho y 2,5 de alto, modificable",
         material: "---",
         tematica: ["llaveros", "abalorios"],
         imagenes: [
@@ -42,9 +42,9 @@ const productos = [
         id: 4,
         titulo: "Bola navidad personalizada",
         precioUnidad: 4.0,
-        precioPack: "No aplica",
-        descripcion: "Placa conmemorativa de gran tamaño con los nombres de todos los alumnos grabados.",
-        material: "---",
+        precioPack: "No aplica pack",
+        descripcion: "ideal para arbol, diametro 9cm",
+        material: "MDF",
         tematica: "navidad",
         imagenes: [
             "img/bolanavidadpersonalizada2.jpg",
@@ -55,9 +55,9 @@ const productos = [
         id: 5,
         titulo: "Imán recuerdo mi primera comunión",
         precioUnidad: 4.0,
-        precioPack: "60€ (Pack de 20 unid.) [-25%]",
-        descripcion: "Imán con --- medidas, foto a elegir del niño.",
-        material: "---",
+        precioPack: "hasta 10 unidades -5% y +20 unidades -10%",
+        descripcion: "Imán con 9cm de alto medidas, foto a elegir del niño.",
+        material: "MDF",
         tematica: ["Bautizo y comunión"],
         imagen: "img/imancomunion.jpg"
     },
@@ -65,9 +65,9 @@ const productos = [
         id: 6,
         titulo: "Letras decoración",
         precioUnidad: 0,
-        precioPack: "Precio depende del tamaño",
-        descripcion: "Letras con tipografia y medidas personalizadas.",
-        material: "---",
+        precioPack: "El precio depende del tamaño",
+        descripcion: "Letras con tipografia y medidas personalizadas. Por letra de 15 cm de alto aproximadamente 17€. Por letra de 25 cm de alto aproximadamente 21€.",
+        material: "MDF",
         tematica: ["Bautizo y comunión", "decoraciones"],
         imagen: "img/letras.jpg"
     },
@@ -76,8 +76,8 @@ const productos = [
         titulo: "Llavero caballo",
         precioUnidad: 2.50,
         precioPack: "21,25€ (Pack de 10 unid.) [-15%]",
-        descripcion: "Llavero silueta de un caballo",
-        material: "---",
+        descripcion: "Llavero silueta de un caballo, UNOS 4CM DE ALTO, incluye su cadenita pra colgar",
+        material: "MDF",
         tematica: "llaveros",
         imagen: "img/llaverocaballo.jpg"
     },
@@ -85,8 +85,8 @@ const productos = [
         id: 8,
         titulo: "Llavero evento puzle",
         precioUnidad: 3.50,
-        precioPack: "52,50€ (Pack de 20 unid.) [-25%]",
-        descripcion: "Llavero con los datos del niño, fecha, hora, lugar, personalizable al 100%",
+        precioPack: "hasta 10 unidades -5% y +20 unidades -10%",
+        descripcion: "Llavero con los datos del niño, fecha, hora, lugar, personalizable al 100%. APROXIMADAMENTE 3,5 CM",
         material: "---",
         tematica: ["llaveros", "Bautizo y comunión"],
         imagenes: [
@@ -97,9 +97,9 @@ const productos = [
     {
         id: 9,
         titulo: "Nombre",
-        precioUnidad: 5,
+        precioUnidad: 8,
         precioPack: "No disponible",
-        descripcion: "Nombre con las medidas ---, se puede pintar",
+        descripcion: "Nombre con las medidas 25 cm de largo, se puede pintar, tamaño personalizable y tipografia de letra tambien",
         material: "---",
         tematica: "decoraciones",
         imagenes: [
@@ -113,9 +113,9 @@ const productos = [
     {
         id: 10,
         titulo: "Llaveros con imagen y nombre",
-        precioUnidad: 4,
+        precioUnidad: 4.5,
         precioPack: "No disponible",
-        descripcion: "---",
+        descripcion: "Las medidas pueden variar en funcion de diseño a gusto.",
         material: "---",
         tematica: ["llaveros", "semana santa"],
         imagenes: [
@@ -128,8 +128,8 @@ const productos = [
         id: 11,
         titulo: "Llavero evento circular",
         precioUnidad: 3.50,
-        precioPack: "---",
-        descripcion: "Llavero con los datos del niño, fecha, hora, lugar, personalizable al 100%",
+        precioPack: "hasta 10 unidades -5% y +20 unidades -10%",
+        descripcion: "Llavero con los datos del niño, fecha, hora, lugar, personalizable al 100%. el de la imagen tiene unos 4,5 cm de diametro.",
         material: "---",
         tematica: ["llaveros", "Bautizo y comunión"],
         imagenes: [
@@ -140,9 +140,9 @@ const productos = [
     {
         id: 12,
         titulo: "Llavero empresa",
-        precioUnidad: 3.50,
-        precioPack: "---",
-        descripcion: "Llavero con el logo de la empresa",
+        precioUnidad: 4,
+        precioPack: "Preguntar descuentos por cantidad",
+        descripcion: "Llavero con el logo de la empresa. El tamaño depende del logo, aproximadamente 4,5-5 cm",
         material: "---",
         tematica: "llaveros",
         imagenes: [
@@ -156,7 +156,7 @@ const productos = [
     {
         id: 13,
         titulo: "Llavero nombre con forma",
-        precioUnidad: 4,
+        precioUnidad: 4.5,
         precioPack: "---",
         descripcion: "Llavero con el nombre / apellido, lo que quiera y la imagen / forma de lo que más le guste, personalizable al 100%",
         material: "---",
@@ -166,9 +166,9 @@ const productos = [
     {
         id: 14,
         titulo: "Llavero nombre relieve",
-        precioUnidad: 3.50,
+        precioUnidad: 5,
         precioPack: "---",
-        descripcion: "---",
+        descripcion: "---. tamaño unos 4,5cm, depende de las letras del nombre",
         material: "---",
         tematica: "llaveros",
         imagen:  "img/llaveronombrerelieve.jpg" 
@@ -178,7 +178,7 @@ const productos = [
         titulo: "Llavero nombre simple",
         precioUnidad: 3.50,
         precioPack: "---",
-        descripcion: "---",
+        descripcion: "--- tamaño unos 5cm, depende de las letras del nombre",
         material: "---",
         tematica: "llaveros",
         imagenes: [
@@ -195,8 +195,8 @@ const productos = [
         titulo: "Llavero nombre simple",
         precioUnidad: 3.50,
         precioPack: "---",
-        descripcion: "---",
-        material: "---",
+        descripcion: "---tamaño unos 6cm, depende de las letras del nombre",
+        material: "MDF lacado en blanco",
         tematica: "llaveros",
         imagenes: [
             "img/llaveronombresimpleblanco.jpg",
@@ -209,10 +209,10 @@ const productos = [
     {
         id: 17,
         titulo: "Llavero nombre simple",
-        precioUnidad: 3.50,
+        precioUnidad: 4.50,
         precioPack: "---",
-        descripcion: "---",
-        material: "---",
+        descripcion: "--- tamaño unos 4 cm, depende de las letras del nombre",
+        material: "METACRILATO",
         tematica: "llaveros",
         imagenes: [
             "img/llaveronombresimplemetraquilato.jpg",
@@ -222,9 +222,9 @@ const productos = [
     {
         id: 18,
         titulo: "Llavero imagen",
-        precioUnidad: 3.50,
+        precioUnidad: 4.0,
         precioPack: "---",
-        descripcion: "---",
+        descripcion: "--- TAMAÑO UNOS 5CM",
         material: "---",
         tematica: "llaveros",
         imagen:  "img/llaverosemanasanta.jpg",
@@ -235,7 +235,7 @@ const productos = [
         precioUnidad: 8,
         precioPack: "---",
         descripcion: "---",
-        material: "---",
+        material: "--- tamaño hay que medirlo. Las piezas consultar disponibilidad y precio.",
         tematica: "Juegos de mesa",
         imagenes: [
             "img/tableroajedez.jpg",
@@ -243,11 +243,11 @@ const productos = [
         ]
     },
     {
-        id: 20,
+        id: 20,        
         titulo: "Bola navidad personalizada",
         precioUnidad: 4.0,
         precioPack: "No aplica",
-        descripcion: "Placa conmemorativa de gran tamaño con los nombres de todos los alumnos grabados.",
+        descripcion: "--- TAMAÑO 9 CM",
         material: "---",
         tematica: "profesores",
         imagen: "img/bolanavidadpersonalizada.jpg"
@@ -255,10 +255,10 @@ const productos = [
     {
         id: 21,
         titulo: "Boligrafos",
-        precioUnidad: 0,
-        precioPack: "No aplica",
+        precioUnidad: 1.80,
+        precioPack: "Preguntar disponibilidad",
         descripcion: "Boligrafo personalizado, ya sea para comunión, boda, regalo de graduación...",
-        material: "---",
+        material: "BAMBÚ, tinta aleatoria (posiblemente negra)",
         tematica: ["profesores", "Bautizo y comunión"],
          imagenes: [
             "img/boligrafobautizo.jpg",
@@ -272,11 +272,11 @@ const productos = [
     },
     {
         id: 22,
-        titulo: "Llaveros caras",
+        titulo: "Llaveros caras y fecha",
         precioUnidad: 0,
-        precioPack: "No aplica",
+        precioPack: "Desde 4,5€ dependiendo de los detalles de la imagen.",
         descripcion: "Llaveros como detalle de comunión o bautizo.",
-        material: "---",
+        material: "MDF lacado en blanco",
         tematica: ["Bautizo y comunión"],
         imagenes: [
             "img/llaveroscaras.jpg",
@@ -286,20 +286,20 @@ const productos = [
     {
         id: 23,
         titulo: "Llavero imagen pintado a mano",
-        precioUnidad: 0,
-        precioPack: "---",
+        precioUnidad: 6,
+        precioPack: "más 10 unidades -5% y +20 unidades -10%",
         descripcion: "---",
-        material: "---",
+        material: "MDF",
         tematica: "llaveros",
         imagen:  "img/llaveroimagencolor.jpg"
     },
     {
         id: 24,
         titulo: "Cuadro familia",
-        precioUnidad: 0,
-        precioPack: "---",
-        descripcion: "---",
-        material: "---",
+        precioUnidad: 35.5,
+        precioPack: "Para más o menos miembros familiares consultar diseño.",
+        descripcion: "---,Grabado y con marco de color negro o blanco a elección, tamaño 40x30 cm",
+        material: "MDF",
         tematica: "decoración",
         imagen:  "img/cuadro.jpg"
     },
